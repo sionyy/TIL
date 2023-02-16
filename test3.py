119e26a30b023caf083b62487a0f293e4721ca74
arctophile <arctophile19@gmail.com>
2023-02-17 08:49:28 +0900

    0217

--- test3.py
+++ test3.py
@@ -1,14 +1,1 @@
-# N,K = map(int(input().split()))
-N=5
-K=3
-
-arr = [[0,0,1,1,1],
-       [1,1,1,1,0],
-       [0,0,1,0,0],
-       [0,1,1,1,1],
-       [1,1,1,0,1]]
-
-cnt=0
-for i in range(N):
-    for j in range(N-K+1):
-        print(arr[i][j:j+3])
+print('tttt' in 'ttttt')
