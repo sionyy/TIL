12cadcc16149a2df648c72ca9c80c4c02d399577
arctophile <arctophile19@gmail.com>
2023-02-27 17:58:13 +0900

    0227

--- test.py
+++ test.py
@@ -1,17 +1,9 @@
-#n개의 주사위를 던졌을 때 나올 수 있는 모든 경우를 출력해 주세요
-
-n=int(input())
-path=[0]*n
-lst = [1,2,3,4,5,6]
-def dice(level):
-    if level == n:
-        for i in range(n):
-            print(path[i],end=' ')
-        print()
-        return
-    else:
-        for i in range(6):
-            path[level] = lst[i]
-            dice(level+1)
-
-dice(0)
+A = 'ABC'
+B = 'ZZZZZABCZZZZZ'
+rlt = A in B
+# if A in B == True:
+if A in B == True:
+    result = 1
+else:
+    result = 0
+print(result)
--- test2.py
+++ test2.py
@@ -1,42 +1,18 @@
-T = int(input())
-for tc in range(1,T+1):
-    N=int(input())
-    arr = [list(map(int,input().split())) for _ in range(N)]
+"""
+A 10
+B 7
+C 5
 
-    dy = [-1,-1,-1,0,0,1,1,1]
-    dx = [-1,0,1,-1,1,-1,0,1]
-    def find(y,x):
-        cnt=0
-        for i in range(8):
-            ny = y+dy[i]
-            nx = x+dx[i]
-            if ny<0 or nx<0 or ny>N-1 or nx>N-1 : continue
-            if arr[y][x] < arr[ny][nx]:
-                cnt+=1
-        return cnt
+AAAAAAAAAA
+BBBBBBBCCC
+CC
+"""
 
-    def MAXMIN(y,x):
-        MAX=0
-        MIN = 999
-        for i in range(8):
-            ny = y+dy[i]
-            nx = x+dx[i]
-            if ny < 0 or nx < 0 or ny > N - 1 or nx > N - 1: continue
-            if arr[ny][nx] > MAX:
-                MAX = arr[ny][nx]
-            MIN = arr[y][x]
-        return MAX-MIN
+full=''
+N=int(input())
+for i in range(N):
+    alpha,num = map(str,input().split())
+    full+=alpha*int(num)
 
-    result=[]
-    ans=0
-    for i in range(N):
-        for j in range(N):
-            if find(i,j) == 8:
-                ans+=1
-                result.append(MAXMIN(i,j))
-    print(result)
-    MAX =0
-    for i in range(len(result)):
-        if MAX < result[i]:
-            MAX = result[i]
-    print(f'#{tc} {ans} {MAX}')
+for i in range(len(full)):
+    print(full[i],end='')
