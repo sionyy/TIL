7458d95e81d0dd0cc0ca3148db688da96b9f49f4
arctophile <arctophile19@gmail.com>
2023-03-10 17:52:41 +0900

    0310

--- test/test.py
+++ test/test.py
@@ -1,31 +1,15 @@
-# 세로 R
-# 가로 C
-result=[]
-def check(y1,x1,y2,x2,y3,x3,y4,x4):
-    for i in range(y2-y1+1):
-        for j in range(x1,x2+1):
-            arr[i][j]+=1
-    for i in range(y4-y3+1):
-        for j in range(x3,x4+1):
-            arr[i][j]+=1
+N=int(input())
 
-    for i in range(MAXX):
-        for j in range(MAXX):
-            if arr[i][j]==2:
-                if arr[i+2][j+2] ==2 or arr[i+2][j-2]==2 or arr[i-2][j+2] ==2 or arr[i-2][j-2] ==2:
-                    return result.append('a')
-                if arr[i+1][j] ==2 or arr[i-1][j]==2 or arr[i][j+1]==2 or arr[i][j-1] == 2:
-                    return result.append('b')
-                if arr[i+1][j+1] ==2 or arr[i+1][j-1]==2 or arr[i-1][j+1] ==2 or arr[i-1][j-1] ==2:
-                    return result.append('c')
-    return result.append('d')
-
-for repeat in range(4):
-    y1,x1,y2,x2,y3,x3,y4,x4=map(int,input().split())
-    MAXX = max(y1,x1,y2,x2,y3,x3,y4,x4)+1
-
-    arr=[[0]*MAXX for _ in range(MAXX)]
-    check(y1,x1,y2,x2,y3,x3,y4,x4)
-
-print(result)
-
+lst=[]
+abslst=[]
+for i in range(N):
+    lst.append(int(input()))
+    abslst.append(abs(lst[-1]))
+    if len(lst)==0:
+        print(0)
+    elif lst[-1]==0:
+        for i in range(len(abslst)):
+            if min(abslst)==abslst[i]:
+                print(lst[i])
+                del lst[i]
+                del abslst[i]
